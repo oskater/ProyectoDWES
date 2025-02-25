3d25b7b32d504f0a64c3fc6f5b9375752d375c1f
oskater <oscarredfer04@gmail.com>
2025-02-25 20:42:49 +0100

    Modificado bien

--- ausospa/dashboard.php
+++ ausospa/dashboard.php
@@ -52,7 +52,23 @@ include_once(__DIR__ . '/views/Header.php');
 
     </article>
 
-    
+    <?php
+    // if ROL==ADMIN (Sacar de sesion)
+    // echo 
+    // (
+        //     <article class="flex flex-col justify-center align-center text-center gap-4 w-[100%] max-w-[400px] mt-4 bg-blue-100 rounded-lg p-4">
+                
+        //     <h1 class="text-center font-extrabold text-xl">Empleados</h1>
+        
+        //     <p class="w-[200px] mx-auto bg-blue-300 rounded-lg hover:bg-blue-500"><a href="./dashboard.php?controller=Empleado&action=listar">Listar Empleados</a></p>
+            
+        //     <p class="w-[200px] mx-auto bg-blue-300 rounded-lg hover:bg-blue-500"><a href="./dashboard.php?controller=Empleado&action=insertar">Añadir Empleado</a></p>
+        
+        //     <p class="w-[200px] mx-auto bg-blue-300 rounded-lg hover:bg-blue-500"><a href="./dashboard.php?controller=Empleado&action=eliminar">Eliminar Empleado</a></p>
+        
+        //      </article>
+    // ) 
+    ?>
 
 </main>
 
